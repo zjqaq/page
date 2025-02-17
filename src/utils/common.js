0ea7e454a9369846b39ae5fb4b2e54717da57ca9
karollamper <3220718477@qq.com>
2025-02-17 12:47:41 +0800

    sk

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,66 +1,58 @@
 export function setMeta(title,description,keywords) {   
-   // 设置标题
-   document.title = title;
+  // 设置标题
+  document.title = title;
 
-   // 设置描述
-   const metaDescription = document.querySelector('meta[name="description"]');
-   if (metaDescription) {
-     metaDescription.setAttribute('content', description);
-   }
+  // 设置描述
+  const metaDescription = document.querySelector('meta[name="description"]');
+  if (metaDescription) {
+    metaDescription.setAttribute('content', description);
+  }
 
-   // 设置关键词
-   const metaKeywords = document.querySelector('meta[name="keywords"]');
-   if (metaKeywords) {
-     metaKeywords.setAttribute('content', keywords);
-   }
+  // 设置关键词
+  const metaKeywords = document.querySelector('meta[name="keywords"]');
+  if (metaKeywords) {
+    metaKeywords.setAttribute('content', keywords);
+  }
 }
 
 export function getFormattedTime(currentDate){
-    return currentDate.toLocaleTimeString('en-US', {
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        hour12: false,
-      }).replace(/:/g, ' : ');
+   return currentDate.toLocaleTimeString('en-US', {
+       hour: '2-digit',
+       minute: '2-digit',
+       second: '2-digit',
+       hour12: false,
+     }).replace(/:/g, ' : ');
 }
 
 export function getFormattedDate(currentDate){
-    const year = currentDate.getFullYear();
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-    const day = String(currentDate.getDate()).padStart(2, '0');
-    const weekday = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'][currentDate.getDay()];
+   const year = currentDate.getFullYear();
+   const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+   const day = String(currentDate.getDate()).padStart(2, '0');
+   const weekday = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'][currentDate.getDay()];
 
-    return `${year} 年 ${month} 月 ${day} 日 ${weekday}`;
+   return `${year} 年 ${month} 月 ${day} 日 ${weekday}`;
 }
 
 export function dataConsole(){
-  console.log(`%c
-    ___       _______   ___       _______   ________         
-    |\\  \\     |\\  ___ \\ |\\  \\     |\\  ___ \\ |\\   __  \\       
-    \\ \\  \\    \\ \\   __/|\\ \\  \\    \\ \\   __/|\\ \\  \\|\\  \\      
-     \\ \\  \\    \\ \\  \\_|/_\\ \\  \\    \\ \\  \\_|/_\\ \\  \\\\\\  \\     
-      \\ \\  \\____\\ \\  \\_|\\ \\ \\  \\____\\ \\  \\_|\\ \\ \\  \\\\\\  \\    
-       \\ \\_______\\ \\_______\\ \\_______\\ \\_______\\ \\_______\\   
-        \\|_______|\\|_______|\\|_______|\\|_______|\\|_______|   
-                 %c                                            
-                 ,----------------,              ,---------, 
-            ,-----------------------,          ,"        ,"| 
-          ,"                      ,"|        ,"        ,"  | 
-         +-----------------------+  |      ,"        ,"    | 
-         |  .-----------------.  |  |     +---------+      | 
-         |  |                 |  |  |     | -==----'|      | 
-         |  |  I LOVE YOU!    |  |  |     |         |      | 
-         |  |  FROM %cleleo.top%c |  |  |/----|'---=    |      | 
-         |  |  Copyright ©    |  |  |   ,/|==== ooo |      ; 
-         |  |      ${new Date().getFullYear()}       |  |  |  // |(((( [33]|    ,"  
-         |  ·-----------------'  |," .;'| |((((     |  ,"    
-         +-----------------------+  ;;  | |         |,"      
-            /_)______________(_/  //'   | +---------+        
-       ___________________________/___  ',                   
-      /  oooooooooooooooo  .o.  oooo /,   \,"-----------      
-     / ==ooooooooooooooo==.o.  ooo= //   ,'\--{)B     ,"      
-    /_==__==========__==_ooo__ooo=_/'   /___________,"       
-    '-----------------------------'                          
-`,'background-color:#CFFFFB','background-color:#CFFFFB','background-color:#CFFFFB;color: #0045FE;','background-color:#CFFFFB');
-    
+ console.log(`%c                                                             
+                ,----------------,              ,---------, 
+           ,-----------------------,          ,"        ,"| 
+         ,"                      ,"|        ,"        ,"  | 
+        +-----------------------+  |      ,"        ,"    | 
+        |  .-----------------.  |  |     +---------+      | 
+        |  |                 |  |  |     | -==----'|      | 
+        |  |  I LOVE YOU!    |  |  |     |         |      | 
+        |  |  FROM %cleleo.top%c |  |  |/----|'---=    |      | 
+        |  |  Copyright ©    |  |  |   ,/|==== ooo |      ; 
+        |  |      ${new Date().getFullYear()}       |  |  |  // |(((( [33]|    ,"  
+        |  ·-----------------'  |," .;'| |((((     |  ,"    
+        +-----------------------+  ;;  | |         |,"      
+           /_)______________(_/  //'   | +---------+        
+      ___________________________/___  ',                   
+     /  oooooooooooooooo  .o.  oooo /,   \,"-----------      
+    / ==ooooooooooooooo==.o.  ooo= //   ,'\--{)B     ,"      
+   /_==__==========__==_ooo__ooo=_/'   /___________,"       
+   '-----------------------------'                          
+`,'background-color:#CFFFFB','background-color:#CFFFFB;color: #0045FE;','background-color:#CFFFFB');
+   
 }
